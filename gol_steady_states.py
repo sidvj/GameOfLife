cd68fe21320aae9c3b895543cebfc458836f6c40
sidvj <siddarth.vijay@outlook.com>
2022-05-08 17:27:31 -0600

    Create gol_steady_states.py

---
+++ gol_steady_states.py
@@ -0,0 +1,159 @@
+import turtle
+import time
+from copy import copy, deepcopy
+
+
+screen = turtle.Screen()
+screen.setworldcoordinates(0,0,500,500)
+turtle.shape("turtle")
+turtle.speed(0)
+dotcolor = "black"
+turtle.hideturtle()
+
+worldarray = [
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,0,1,0,1,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,0,1,0,1,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+              ]
+ 
+print ("len of worldarray: " , len(worldarray))
+print ("len of worldarray[0]:", len(worldarray[0]))
+
+copy_of_worldarray = deepcopy(worldarray)
+
+def applyOppositeRule():
+    #print ("Appling opposite rule")
+    ## do stuff here
+    #print ("sid worldarray num is:",worldarray[0][0])
+
+    for x in range(len(worldarray)): # outer loop // x: 0
+        for y in range(len(worldarray)):
+            if worldarray[x][y] == 0:
+                worldarray[x][y] = 1
+            else:
+                worldarray[x][y] = 0
+
+def findNeighbors(x,y):
+    # input is position 2,2
+    ############### Input: worldarray[x][y]
+    ############### Input: worldarray[2][2]
+    sum = 0
+    '''print ("neighbor 1: ", worldarray[x-1][y-1])
+    print ("neighbor 2: ", worldarray[x-1][y])
+    print ("neighbor 3: ", worldarray[x-1][y+1])
+    print ("neighbor 4: ", worldarray[x][y-1])
+    print ("neighbor 5: ", worldarray[x][y+1])
+    print ("neighbor 6: ", worldarray[x+1][y-1])
+    print ("neighbor 7: ", worldarray[x+1][y])
+    print ("neighbor 8: ", worldarray[x+1][y+1])'''
+
+    sum = worldarray[x-1][y-1]+worldarray[x-1][y]+worldarray[x-1][y+1]+worldarray[x][y-1]+worldarray[x][y+1]+worldarray[x+1][y-1]+worldarray[x+1][y]+worldarray[x+1][y+1]
+
+    #print ("Total neighbors alive: ", sum)
+    #Each cell with one or no neighbors dies
+    #Each cell with four or more neighbors dies
+    #Each cell with two or three neighbors survives.
+    if worldarray[x][y]==1: # current spot is alive
+      if (sum <= 1): # death due to solitude
+        #print ("Current position dies")
+        copy_of_worldarray[x][y]=0
+      elif (sum >= 4): # death due to overpopulation
+        #print ("Current position dies")
+        copy_of_worldarray[x][y]=0
+      else:
+        #print(">>>>>>>>>>> Current position lives")
+        copy_of_worldarray[x][y]=1
+    else: # curret spot is dead
+      if (sum == 3):
+        #print ("current position born")
+        copy_of_worldarray[x][y]=1
+
+def walkAllInternalSpots():
+    for x in range(len(worldarray)):
+        for y in range(len(worldarray)):
+            #print (" x is ", x , "y is ", y)
+            if x==0:
+                continue
+            elif y==0:
+                continue
+            elif x == len(worldarray)-1:
+              continue
+            elif y==len(worldarray)-1:
+              continue
+            else:
+              findNeighbors(x, y)
+
+def applyConwayRule():
+  walkAllInternalSpots()
+
+y = 0
+turtle.penup()
+
+def draw_dots_horizontal():
+    for x in range(len(worldarray)):
+        turtle.setx(x*10)
+        turtle.sety(y*10)
+        life = worldarray[x][y]
+        #print ("value of mydatamulti[", x , "," , y ,"]: ", life)        
+        #print ("turtles posision is : ", turtle.pos()) # 1, 1
+        if (life == 1):
+            turtle.dot(10, "black")
+            turtle.penup()
+        else:
+            turtle.dot(10, "white")
+            turtle.penup()
+
+def draw_world():
+    global y
+    for y in range(len(worldarray[0])):
+        #print (" y is ", y)
+        draw_dots_horizontal()
+
+for loop in range(200):
+    turtle.tracer(0, 0)
+    draw_world()
+    turtle.update()
+
+    #time.sleep(0.5)
+    applyConwayRule()
+    worldarray=deepcopy(copy_of_worldarray)
+
+
+turtle.exitonclick()
